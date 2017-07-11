f37a8b5a5f49ca4dde4008dcfd6ff2e67d9f9071
sunitha muthukrishna <sumuth@microsoft.com>
2017-07-10 21:08:39 -0700

    updated DB information in wpisettings.php

--- mw-config/WpiSettings.php
+++ mw-config/WpiSettings.php
@@ -1,19 +1,11 @@
 <?php
 
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
+
+    $connectstr_dbhost = getenv('DATABASE_HOST');
+    $connectstr_dbname = getenv('DATABASE_NAME');;
+    $connectstr_dbusername = getenv('DATABASE_USERNAME');;
+    $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
+
 
 $hostname = gethostname();
 
